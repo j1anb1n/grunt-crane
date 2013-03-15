01df8443cc639f7fe71f416190d96ca421463ecf
j1anb1n <chan.jianbin@gmail.com>
2013-03-15 13:48:43 +0800

    add .cmb.js builder

--- tasks/build_js.js
+++ tasks/build_js.js
@@ -4,6 +4,18 @@ module.exports = function (grunt) {
     var minify  = grunt.config('minify');
     var from    = grunt.config('src');
     var to      = grunt.config('dest');
+    var mapFile = grunt.config('temp') + '/js_cmb_map.json';
+
+    var map     = {
+        "child": {},
+        "parent": {}
+    };
+
+    try {
+        map = grunt.file.readJSON(mapFile);
+    } catch (ex) {
+        grunt.file.write(mapFile, map);
+    }
 
     grunt.registerTask('build-js', 'Transport js files', function () {
         var files = [].slice.call(arguments);
@@ -12,6 +24,31 @@ module.exports = function (grunt) {
     });
 
     function builder (id) {
+        var content = '';
+        if (isCmbFile(id)) {
+            var content = buildCmbJS(id);
+        } else {
+            var content = buildJS(id);
+        }
+
+        grunt.file.write(to + id, content);
+
+        if (map.child[id]) {
+            map.child[id] = map.child[id]
+                .filter(function (f) {
+                    if (!grunt.file.exists(from + f)) {
+                        delete map.parent[id];
+                        return false;
+                    }
+                    return true;
+                });
+            grunt.task.run(['build-js:' + map.child[id].join(':')]);
+        }
+        grunt.file.write(mapFile, JSON.stringify(map, null, 4));
+    }
+
+    function buildJS(id) {
+        var match = [];
         var content = grunt.file.read(from + id);
         var deps = [];
         REQUIRE_RE.lastIndex = 0;
@@ -27,7 +64,59 @@ module.exports = function (grunt) {
         if (minify) {
             content = grunt.helper('uglify', content);
         }
+        return content;
+    }
 
-        grunt.file.write(to + id, content);
+    function buildCmbJS(id) {
+        var content = grunt.file.read(from + id);
+
+        var imports = content.split('\n');
+        content = imports
+            .filter(function (f) {
+                return !!f.trim();
+            })
+            .map(function (f) {
+                if (!grunt.file.exists(from + f)) {
+                    throw new Error('File no exist:'+f);
+                }
+                return buildJS(f);
+            })
+            .join('\n');
+
+        imports.forEach(function (file) {
+            var child = map.child[file];
+            if (child && Array.isArray(child) && child.indexOf(id) === -1) {
+                child.push(id);
+            } else {
+                map.child[file] = [id];
+            }
+        });
+
+        // if an `@import` was deleted, then it should be removed from the map
+        if (map.parent[id]) {
+            map.parent[id]
+                .filter(function (child) {
+                    return imports.indexOf(child) === -1;
+                })
+                .forEach(function (file) {
+                    var child = map.child[file];
+                    if (child && Array.isArray(child) && child.indexOf(id) !== -1) {
+                        child.splice(child.indexOf(id), 1);
+                        if (!child.length) {
+                            delete map.child[file];
+                        }
+                    }
+                });
+        }
+        if (imports.length) {
+            map.parent[id] = imports;
+        } else {
+            delete map.parent[id];
+        }
+        return content;
+    }
+
+    function isCmbFile (id) {
+        return (/cmb\.js$/).test(id);
     }
 };
