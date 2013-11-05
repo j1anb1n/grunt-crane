a9aab6cd6b5622a1c8177f1461ab49876aeaaf2c
陈建镔 <chenjianbin@ganji.com>
2013-11-05 13:56:28 +0800

    fs.watch逻辑使用gaze完成

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,65 +1,30 @@
+var path = require('path');
 var spawn = require('child_process').spawn;
-var mtimes = {};
+var Gaze = require('gaze').Gaze;
 
 module.exports = function(grunt) {
-    var fs = require('fs');
     grunt.registerTask('watch', function() {
         this.requiresConfig('watch');
 
         var src = grunt.config('src');
 
-        var files = {};
-        var timer = null;
-
         this.async();
 
-        grunt.log.subhead('Waiting...');
+        var gaze = new Gaze(src + '/**/*');
 
-        getFiles().forEach(watchFile);
+        grunt.log.subhead('Watching...');
 
-        timer = setInterval(function () {
-            grunt.util._.difference(getFiles(), Object.keys(files))
-                .forEach(function (file) {
-                    grunt.log.writeln('ADD: '.blue + file);
-                    onChange(file);
-                    watchFile(file);
-                });
-        }, 200);
+        gaze.on('all', function (event, filepath) {
+            filepath = filepath.replace(path.resolve(src), '');
+            grunt.log.writeln('%s: %s', event.toUpperCase(), filepath);
 
-        function getFiles () {
-            return grunt.file.expand({
-                filter: function (file) {
-                    return grunt.file.isFile(file);
-                }
-            }, src + '**/*');
-        }
-
-        function watchFile (file) {
-            if (!files[file]) {
-                files[file] = fs.watch(file, function () {
-                    onChange(file);
-                });
+            if (event !== 'deleted') {
+                build(filepath);
             }
-        }
+        });
 
-        function onChange (file) {
-            if (!fs.existsSync(file)) {
-                files[file].close();
-                grunt.log.writeln('REMOVE '.red + file);
-                delete files[file];
-                delete mtimes[file];
-                return;
-            }
-
-            var mtime = +fs.statSync(file).mtime;
-
-            if (mtimes[file] === mtime) {
-                return;
-            }
-
-            mtimes[file] = mtime;
-
-            var child = spawn('grunt', ['build:' + file.replace(src, ''), 'deploy']);
+        function build(file) {
+            var child = spawn('grunt', ['build:' + file]);
             var output = '', errorMsg = '';
 
             child.stdout.on('data', function (data) {
